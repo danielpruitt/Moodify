301f348e68a36b800020f5f4cc6e0380eb2c7bb3
Andrew Park <34328939+apark5040@users.noreply.github.com>
2018-07-27 16:18:38 -0400

    Delete keys.js

--- assets/keys.js
+++
@@ -1,7 +0,0 @@
-var keys= {
-    spotify_id:"2752cb9f8d0940aeb25e5c564dd68a1e",
-    spotify_secret:"07c7345aa3c6424289bb28e7e27b919f",
-    kairos_id:'5989e8db',
-    kairos_key:'f74c4a76f8186a5c54d2afbe34015740',
-    imgur_id:'client-ID c98e83d1fb7401a'
-}
